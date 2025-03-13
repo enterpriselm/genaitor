e7b67a3e81c82185140ce84ac5a7a2a9b25e250d
Atyson Jaime <atysonjaime@gmail.com>
2025-03-12 23:52:31 -0300

    feat: Add principal index.

--- frontend/genaitor-frontend/src/app/page.tsx
+++ frontend/genaitor-frontend/src/app/page.tsx
@@ -1,9 +1,24 @@
 "use client"
 
+import { Tab, Tabs } from "react-bootstrap"
+import { ContentSection } from "./styled"
+
 export default function Index() {
 	return (
-		<div className="flex flex-col items-center justify-center">
-			<h1>Index</h1>
-		</div>
+		<ContentSection>
+			<h6>Choose what do you want</h6>
+			<div className="overflow-auto">
+				<Tabs
+					defaultActiveKey="agents"
+					id="tabs-content-option-genaitor"
+					className="tabs-genaitor"
+				>
+					<Tab eventKey="agents" title="Use Preset Agents"></Tab>
+					<Tab eventKey="flows" title="Use Preset Flows"></Tab>
+					<Tab eventKey="create-flow" title="Create new Flow"></Tab>
+					<Tab eventKey="create-agent" title="Create New Task and Agent"></Tab>
+				</Tabs>
+			</div>
+		</ContentSection>
 	)
 }
