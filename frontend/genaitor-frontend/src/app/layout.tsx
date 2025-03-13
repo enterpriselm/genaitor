e60338daf50939ab4661adef34b4fab4387ed98d
Atyson Jaime <atysonjaime@gmail.com>
2025-03-12 23:20:22 -0300

    feat: Add new config to the frontend.

---
+++ frontend/genaitor-frontend/src/app/layout.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import { Nunito } from "next/font/google"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "@/assets/css/globals.css"
+import "react-toastify/dist/ReactToastify.css"
+import StyledComponentsRegistry from "./registry"
+import { ToastContainer } from "react-toastify"
+import React from "react"
+
+const nunito = Nunito({
+	subsets: ["latin"],
+	variable: "--font-nunito",
+})
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	React.useEffect(() => {
+		// eslint-disable-next-line @typescript-eslint/no-require-imports
+		require("bootstrap/dist/js/bootstrap.bundle.min.js")
+	}, [])
+
+	return (
+		<html lang="en">
+			<head>
+				<title>Genaitor Frontend</title>
+				<link rel="icon" href="/favicon.ico" />
+			</head>
+			<body className={`${nunito.variable}`}>
+				<ToastContainer
+					position="top-right"
+					hideProgressBar={false}
+					newestOnTop={false}
+					closeOnClick
+					pauseOnFocusLoss
+					draggable
+					pauseOnHover
+					theme="colored"
+					limit={1}
+				/>
+				<StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+			</body>
+		</html>
+	)
+}
---
+++ frontend/genaitor-frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function Index() {
+	return (
+		<div className="flex flex-col items-center justify-center">
+			<h1>Index</h1>
+		</div>
+	)
+}
