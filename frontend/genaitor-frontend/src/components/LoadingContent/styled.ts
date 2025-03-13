7c15dc6fdfccb44f7cd26234c4dadaea87bdd983
Atyson Jaime <atysonjaime@gmail.com>
2025-03-13 00:48:49 -0300

    feat: Add loading component

---
+++ frontend/genaitor-frontend/src/components/LoadingContent/styled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+export const LoadingContentStyled = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	gap: 1rem;
+	justify-content: center;
+	align-items: center;
+
+	.spinner-border {
+		width: 3rem;
+		height: 3rem;
+	}
+`
