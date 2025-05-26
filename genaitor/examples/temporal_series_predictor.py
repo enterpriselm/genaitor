a1a9bc7a4d340f00a9f8c38ef0bc1d27c8662ed3
Yan Barros <108139858+barrosyan@users.noreply.github.com>
2025-05-26 11:50:32 -0300

    Update temporal_series_predictor.py

--- genaitor/examples/temporal_series_predictor.py
+++ genaitor/examples/temporal_series_predictor.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import asyncio
 import os
-import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from core import Orchestrator, Flow, ExecutionMode
-from presets.agents import feature_selection_agent, signal_analysis_agent, residual_evaluation_agent, lstm_model_agent, lstm_residual_evaluation_agent
+from genaitor.core import Orchestrator, Flow, ExecutionMode
+from genaitor.presets.agents import feature_selection_agent, signal_analysis_agent, residual_evaluation_agent, lstm_model_agent, lstm_residual_evaluation_agent
 
 async def main():
     print("\nInitializing Time Series Analysis System...")
