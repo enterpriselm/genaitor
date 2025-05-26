d5e19cc4a7dac9845d98b317b57f8a143f2c4891
Yan Barros <108139858+barrosyan@users.noreply.github.com>
2025-05-26 11:47:38 -0300

    Update anomalies_detection.py

--- genaitor/examples/anomalies_detection.py
+++ genaitor/examples/anomalies_detection.py
@@ -1,13 +1,11 @@
 import pandas as pd
 import asyncio
 import os
-import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from core import (
+from genaitor.core import (
     Orchestrator, Flow, ExecutionMode
 )
-from presets.agents import data_understanding_agent, statistics_agent, anomalies_detection_agent, data_analysis_agent
+from genaitor.presets.agents import data_understanding_agent, statistics_agent, anomalies_detection_agent, data_analysis_agent
 
 async def main():
     print("\nInitializing Anomalies Detection System...")
