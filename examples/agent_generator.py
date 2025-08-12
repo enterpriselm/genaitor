a95ca84c785393b95ba16739205d179359ca7c2f
Yan Barros <108139858+barrosyan@users.noreply.github.com>
2025-08-12 12:52:11 -0300

    Update agent_generator.py

--- examples/agent_generator.py
+++ examples/agent_generator.py
@@ -1,5 +1,4 @@
 import asyncio
-import os
 
 from genaitor.core import (
     Orchestrator, Flow, ExecutionMode
@@ -64,6 +63,6 @@ async def main(agent_creation):
             break
 
 if __name__ == "__main__":
-    provider = create_gemini_provider(["GEMINII API KEY"])
+    provider = create_gemini_provider([os.getenv("GEMINI_API_KEY")])
     preset_agents = create_preset_agents(provider)
     asyncio.run(main(preset_agents["agent_creation"]))
